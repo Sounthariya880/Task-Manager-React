70aa5cf119bbb3b008e4cc6e6ab6e7c89b59bfa4
Sounthariya880 <sounthariyamurugan@gmail.com>
2025-07-28 19:53:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Taskmanager from './components/Taskmanager';
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Electronics from './Pages/Electronics';
-import Books from './Pages/Books';
-import Electronicsdetailpage from './Pages/Electronicsdetailpage';
 
 
 function App() {
@@ -14,14 +8,7 @@ function App() {
 
   return (
     <>
-   {/* <Routes>
-    <Route path="/" element={<Home/>}/>
-     <Route path="/Electronics" element={<Electronics />}>
-        <Route path=":id" element={<Electronicsdetailpage />} />
-      </Route>
-        <Route path="/Books" element={<Books/>}/>
-   </Routes>
-     */}
+
      <Taskmanager/>
   
    
